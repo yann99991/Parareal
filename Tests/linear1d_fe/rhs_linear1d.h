b5407545329696e3642100e765435602960f6069
Yann Dreze <drezeyann@gmail.com>
2024-03-21 15:13:54 +0000

    Comments to rhs_linear

--- Tests/linear1d_fe/rhs_linear1d.h
+++ Tests/linear1d_fe/rhs_linear1d.h
@@ -5,10 +5,21 @@
 
 #define LAMBDA -1
 
-// u' = (LAMBDA)*u
+// 
+
+// VectorXd represents a vector of dynamic size. 
+/**
+ * @brief Calculates the rhs of the linear 1D ODE system.
+ *        u' = (LAMBDA)*u
+ * @param t The current time.
+ * @param yf The vector containing the function values.
+ * @param dydt The vector to store the calculated rhs.
+ * @return int Returns 0 on success.
+ */
 inline int 
 linear1d(double t, Eigen::VectorXd &yf, Eigen::VectorXd &dydt)
 {
+  // 0 is the current time index
   dydt(0) = LAMBDA*yf(0);
   return 0;
 }
