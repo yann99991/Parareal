943021e6234d35973a15c09c66ecaa59cf27a4f0
Yann Dreze <drezeyann@gmail.com>
2024-03-21 15:12:31 +0000

    Comments for the forward Euler routines

--- include/Parareal/forward_euler.cpp
+++ include/Parareal/forward_euler.cpp
@@ -1,20 +1,44 @@
 #include "forward_euler.h"
 
+/**
+ * @brief Performs a forward Euler integration of the ODE system 
+ *        untik the final time is reached.
+ * 
+ * @param sys The ODE system to solve.
+ * @param dt The time step size.
+ * @param yf The vector to store the final solution.
+ * @return 0 if the algorithm completes successfully.
+ */
 inline int 
 forward_euler(ode_system &sys, double dt, Eigen::VectorXd &yf)
 {
   yf = sys.y0;
   Eigen::VectorXd dydt(sys.dimension);
   double t = sys.t_init;
+  
+  // if the current time is less than the final time, keep integrating
   while (t + dt/2 < sys.t_final)
   {
+    // Compute the rhs of the ODE system and place it in dydt
     sys.f(t, yf, dydt);
+
+    // Update the solution vector and time
     yf = yf + dt*dydt;
     t = t + dt;
   }
   return 0;
 }
 
+/**
+ * @brief Performs a forward Euler integration of the ODE system 
+ *        until the final time is reached and stores the solution
+ *        at each time step into steps.
+ * 
+ * @param sys The ODE system to solve.
+ * @param dt The time step size.
+ * @param steps The matrix to store the solution at each time step.
+ * @return 0 if the algorithm completes successfully.
+ */
 inline int 
 forward_euler_allt(ode_system &sys, double dt, Eigen::MatrixXd &steps)
 {
